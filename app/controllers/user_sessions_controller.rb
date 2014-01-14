b2e5af8685d0898fd8f7c0fc59028c02aa6a65b7
Nick Hoffman <nick@deadorange.com>
2014-01-13 22:22:55 -0500

    Users can login and logout.

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,21 @@
+class UserSessionsController < ApplicationController
+  before_action :require_login, :only => [:destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:email], params[:password])
+      redirect_back_or_to :users, notice: 'You logged in.'
+    else
+      flash.now[:alert] = "The email and password don't match."
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to :login, notice: 'You logged out.'
+  end
+end
