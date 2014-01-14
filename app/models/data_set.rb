bda949e3e693e21e2404b4e27701b9b98469a8ca
Nick Hoffman <nick@deadorange.com>
2014-01-14 16:53:07 -0500

    Update the DataSet model with fields for "buy" and "sell" prices.

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -1,10 +1,24 @@
 class DataSet < ActiveRecord::Base
-  monetize :cavirtex_cad_cents,     :allow_nil => true
-  monetize :cavirtex_usd_cents,     :allow_nil => true
-  monetize :mtgox_cents,            :allow_nil => true
-  monetize :coinbase_cents,         :allow_nil => true
-  monetize :bitstamp_cents,         :allow_nil => true
-  monetize :btce_cents,             :allow_nil => true
-  monetize :campbx_cents,           :allow_nil => true
-  monetize :cad_to_usd_rate_cents,  :allow_nil => true
+  monetize :cavirtex_cad_buy_cents,  :allow_nil => true
+  monetize :cavirtex_cad_sell_cents, :allow_nil => true
+
+  monetize :cavirtex_usd_buy_cents,  :allow_nil => true
+  monetize :cavirtex_usd_sell_cents, :allow_nil => true
+
+  monetize :mtgox_buy_cents,         :allow_nil => true
+  monetize :mtgox_sell_cents,        :allow_nil => true
+
+  monetize :coinbase_buy_cents,      :allow_nil => true
+  monetize :coinbase_sell_cents,     :allow_nil => true
+
+  monetize :bitstamp_buy_cents,      :allow_nil => true
+  monetize :bitstamp_sell_cents,     :allow_nil => true
+
+  monetize :btce_buy_cents,          :allow_nil => true
+  monetize :btce_sell_cents,         :allow_nil => true
+
+  monetize :campbx_buy_cents,        :allow_nil => true
+  monetize :campbx_sell_cents,       :allow_nil => true
+
+  monetize :cad_to_usd_rate_cents,   :allow_nil => true
 end
--- db/migrate/20140114195617_create_data_sets.rb
+++ db/migrate/20140114195617_create_data_sets.rb
@@ -1,14 +1,28 @@
 class CreateDataSets < ActiveRecord::Migration
   def change
     create_table :data_sets do |t|
-      t.money :cavirtex_cad,    :amount => {:null => true, :default => nil}, :currency => {:present => false}
-      t.money :cavirtex_usd,    :amount => {:null => true, :default => nil}, :currency => {:present => false}
-      t.money :mtgox,           :amount => {:null => true, :default => nil}, :currency => {:present => false}
-      t.money :coinbase,        :amount => {:null => true, :default => nil}, :currency => {:present => false}
-      t.money :bitstamp,        :amount => {:null => true, :default => nil}, :currency => {:present => false}
-      t.money :btce,            :amount => {:null => true, :default => nil}, :currency => {:present => false}
-      t.money :campbx,          :amount => {:null => true, :default => nil}, :currency => {:present => false}
-      t.money :cad_to_usd_rate, :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :cavirtex_cad_buy,  :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :cavirtex_cad_sell, :amount => {:null => true, :default => nil}, :currency => {:present => false}
+
+      t.money :cavirtex_usd_buy,  :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :cavirtex_usd_sell, :amount => {:null => true, :default => nil}, :currency => {:present => false}
+
+      t.money :mtgox_buy,         :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :mtgox_sell,        :amount => {:null => true, :default => nil}, :currency => {:present => false}
+
+      t.money :coinbase_buy,      :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :coinbase_sell,     :amount => {:null => true, :default => nil}, :currency => {:present => false}
+
+      t.money :bitstamp_buy,      :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :bitstamp_sell,     :amount => {:null => true, :default => nil}, :currency => {:present => false}
+
+      t.money :btce_buy,          :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :btce_sell,         :amount => {:null => true, :default => nil}, :currency => {:present => false}
+
+      t.money :campbx_buy,        :amount => {:null => true, :default => nil}, :currency => {:present => false}
+      t.money :campbx_sell,       :amount => {:null => true, :default => nil}, :currency => {:present => false}
+
+      t.money :cad_to_usd_rate,   :amount => {:null => true, :default => nil}, :currency => {:present => false}
 
       t.timestamps
     end
